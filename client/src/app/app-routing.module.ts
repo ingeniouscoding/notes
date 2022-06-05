9ab47f82634039bf749048b82a914fbf2a0d8e3e
dimchik_st@list.ru <dimchik_st@list.ru>
2022-06-05 17:47:42 +0300

    add routing to auth module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,22 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './core/components/home-page/home-page.component';
-import { LoginPageComponent } from './auth/components/login-page/login-page.component';
-import { RegisterPageComponent } from './auth/components/register-page/register-page.component';
 import { NotFoundPageComponent } from './core/components/not-found-page/not-found-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePageComponent,
-  },
-  {
-    path: 'login',
-    component: LoginPageComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterPageComponent,
   },
   {
     path: '**',

21e40a9303cad3617eae0982f9e741686af55d3c
dimchik_st@list.ru <dimchik_st@list.ru>
2022-06-06 18:01:52 +0300

    return partial user

--- server/routes/api.php
+++ server/routes/api.php
@@ -15,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+    return [
+        'name'  => $request->user()->name,
+        'email' => $request->user()->email,
+    ];
 });
